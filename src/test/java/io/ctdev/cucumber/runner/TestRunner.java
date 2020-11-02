91a137e987a6ff091f13197ea6edea200e2ea82b
romanovichmax <maksym.vasylchenko1@gmail.com>
2020-11-02 20:16:14 +0200

    Lesson 8

--- src/test/java/io/ctdev/cucumber/runner/TestRunner.java
+++ src/test/java/io/ctdev/cucumber/runner/TestRunner.java
@@ -6,8 +6,6 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         plugin = "pretty",
-        monochrome = true,
-        tags = "smoke",
         glue = "io.ctdev.cucumber",
         features = "src/test/java/io/ctdev/cucumber/features/SingUp.feature")
 
